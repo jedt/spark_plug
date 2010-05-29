1be83d4dc45e4ac9b679f9194fcd096004493cd8
steinkel <steinkel@gmail.com>
2010-05-29 20:19:13 +0800

    cache integration (need to improve)

--- models/user_group_permission.php
+++ models/user_group_permission.php
@@ -3,5 +3,9 @@ class UserGroupPermission extends SparkPlugAppModel
 {
 	var $name = 'UserGroupPermission';
     var $belongsTo = array('SparkPlug.UserGroup');
+    
+    //TODO: terminar estas 2 y upload !
+    // aftersave - crear entrada de cache para reglas del grupo
+    // afterdelete borrar entrada de cache para el grupo
 }
 ?>
